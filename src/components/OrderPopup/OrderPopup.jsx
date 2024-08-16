c6a7548dd1e0a262738329a30cdac595ad5645cd
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-08-16 21:33:12 +0530

    order page edited

--- src/components/OrderPopup/OrderPopup.jsx
+++ src/components/OrderPopup/OrderPopup.jsx
@@ -31,13 +31,28 @@ const OrderPopup = ({ orderPopup, setOrderPopup }) => {
               />
               <input
                 type="email"
-                placeholder="email"
+                placeholder="Mobile Number"
                 className="w-full rounded-full border border-gray-300 dark:border-gray-500 dark:bg-gray-800 px-2 py-1 mb-4"
               />
               <input
                 type="text"
-                placeholder="Address"
+                placeholder="Age"
                 className="w-full rounded-full border border-gray-300 dark:border-gray-500 dark:bg-gray-800 px-2 py-1 mb-4"
+              />
+              <input
+                  type="text"
+                  placeholder="Address"
+                  className="w-full rounded-full border border-gray-300 dark:border-gray-500 dark:bg-gray-800 px-2 py-1 mb-4"
+              />
+              <input
+                  type="text"
+                  placeholder="Email"
+                  className="w-full rounded-full border border-gray-300 dark:border-gray-500 dark:bg-gray-800 px-2 py-1 mb-4"
+              />
+              <input
+                  type="text"
+                  placeholder="City"
+                  className="w-full rounded-full border border-gray-300 dark:border-gray-500 dark:bg-gray-800 px-2 py-1 mb-4"
               />
               <div className="flex justify-center">
                 <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-1 px-4 rounded-full ">
