8ac60377db43ad37a2906e5b4324500a3e9cb050
PrabhaWijera <prabhashwijerathna2001@gmail.com>
2024-08-17 04:19:46 +0530

    update home and nav add imgeess for home cards places

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Hero from "../components/Hero/Hero";
-import NatureVid from "../assets/video/main.mp4";
-import BlogsComp from "../components/Blogs/BlogsComp";
+
 import Places from "../components/Places/Places";
 import Testimonial from "../components/Testimonial/Testimonial";
 import Banner from "../components/Banner/Banner";
@@ -10,6 +8,7 @@ import BannerImg from "../assets/cover-women.jpg";
 import Banner2 from "../assets/travel-cover2.jpg";
 import OrderPopup from "../components/OrderPopup/OrderPopup";
 import homeBg from '../assets/img.png'
+import BlogsComp from "../components/Blogs/BlogsComp.jsx";
 const Home = () => {
   const [orderPopup, setOrderPopup] = React.useState(false);
 
@@ -17,31 +16,29 @@ const Home = () => {
     setOrderPopup(!orderPopup);
   };
   return (
-    <>
-      <div>
-        <div className="h-[570px] relative">
-          {/* <video
-            autoPlay
-            loop
-            muted
-            className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"
-          >
-            <source src={NatureVid} type="video/mp4" />
-          </video> */}
-          <img src={homeBg} alt=""  className="absolute right-0 top-0 h-[700px]display-cover" />
-          {/* <Hero /> */}
+      <>
+        <div>
+          <div className="h-[700px] relative">
+            {/*<video*/}
+            {/*    autoPlay*/}
+            {/*    loop*/}
+            {/*    muted*/}
+            {/*    className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"*/}
+            {/*>*/}
+            {/*  <source src={homeBg} type="video/mp4" />*/}
+            {/*</video>*/}
+            <img src={homeBg} alt="" className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"/>
+            {/*/!*<Hero />*!/*/}
+          </div>
+          <Places handleOrderPopup={handleOrderPopup} />
+          <BannerPic img={BannerImg} />
+          <BlogsComp />
+          <Banner />
+          <BannerPic img={Banner2} />
+          <Testimonial />
+          <OrderPopup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
         </div>
-       
-        <Places handleOrderPopup={handleOrderPopup} />
-        <BannerPic img={BannerImg} />
-        {/* <BlogsComp /> */}
-        <Banner />
-        <Testimonial />
-        <BannerPic img={Banner2} />
-      
-        <OrderPopup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
-      </div>
-    </>
+      </>
   );
 };
 
