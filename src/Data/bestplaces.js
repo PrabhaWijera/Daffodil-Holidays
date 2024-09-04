9d994e1a0cdc2cea429fff8afd6a9b190a15e0e1
PrabhaWijera <prabhashwijerathna2001@gmail.com>
2024-09-05 02:14:17 +0530

    update

---
+++ src/Data/bestplaces.js
@@ -0,0 +1,136 @@
+import Img1 from "../assets/places/Anuradhapura.jpg";
+import Img2 from "../assets/places/Polonnaruwa.jpg";
+import Img3 from "../assets/places/Sigiriya.jpg";
+import Img4 from "../assets/places/selected images for daffodil/kingdom4 resized.webp";
+import Img5 from "../assets/places/selected images for daffodil/greenNature.jpg";
+import Img6 from "../assets/colombo/IMG_0686 copy.webp";
+import Img7 from "../assets/places/selected images for daffodil/fishing resized.webp";
+import Img8 from "../assets/colombo/IMG_0979 copy.webp";
+import Img9 from "../assets/places/Kalpitiya.jpg";
+import Img10 from "../assets/places/selected images for daffodil/galle_resized.webp";
+import Img11 from "../assets/places/selected images for daffodil/mirissa resized.webp";
+import Img12 from "../assets/places/Tangalle.jpg";
+import Img13 from "../assets/places/selected images for daffodil/trincomalee copy.webp";
+import Img14 from "../assets/places/Arugam Bay.jpg";
+import Img15 from "../assets/places/Pasikudah.jpg";
+
+const bestplaces = {
+    AncientCities: [
+        {
+            id:'Polonnaruwa',
+            img: Img1,
+            title: "Polonnaruwa",
+            location: "Sri Lanka",
+            description: " Medieval capital, majestic ruins, and rich cultural history."
+        },
+        {
+            id: 'Anuradhapura',
+            img: Img2,
+            title: "Anuradhapura",
+            location: "Sri Lanka",
+            description: " Ancient capital, sacred stupas, and rich Buddhist heritage.."
+        },
+        {
+            id:'Sigiriya',
+            img: Img3,
+            title: "Sigiriya",
+            location: "Sri Lanka",
+            description: " Ancient rock fortress, royal palace, and stunning frescoes.."
+        }
+    ],
+    HillCountry: [
+        {
+            id:'Kandy',
+            img: Img4,
+            title: "Kandy",
+            location: "Sri Lanka",
+            description: " Last kingdom, sacred Temple, and vibrant cultural heritage.."
+        },
+        {
+            id:'Nuwara',
+            img: Img5,
+            title: "Nuwara Eliya",
+            location: "Sri Lanka",
+            description: " Colonial hill station, cool climate, and tea heritage.."
+        },
+        {
+            id:'Ella',
+            img: Img6,
+            title: "Ella",
+            location: "Sri Lanka",
+            description: " Scenic town, ancient temples, colonial heritage, and stunning landscapes.."
+        }
+    ],
+    Westcoast: [
+        {
+            id:'Negombo',
+            img: Img7,
+            title: "Negombo",
+            location: "Sri Lanka",
+            description: " Historic port city with colonial heritage and vibrant fishing culture.."
+        },
+        {
+            id:'Colombo',
+            img: Img8,
+            title: "Colombo",
+            location: "Sri Lanka",
+            description: " Sri Lanka's bustling capital with rich colonial history and culture.."
+        },
+        {
+            id:'Kalpitiya',
+            img: Img9,
+            title: "Kalpitiya",
+            location: "Sri Lanka",
+            description: "Coastal town with Dutch colonial history and stunning natural beauty.."
+        }
+    ],
+    DownSouth: [
+        {
+            id:'Galle',
+            img: Img10,
+            title: "Galle",
+            location: "Sri Lanka",
+            description: " Historic port city with Dutch fort and colonial architectural heritage.."
+        },
+        {
+            id:'Mirissa',
+            img: Img11,
+            title: "Mirissa",
+            location: "Sri Lanka",
+            description: "Coastal town with historic port, vibrant fishing culture, and beauty.."
+        },
+        {
+            id:'Tangalle',
+            img: Img12,
+            title: "Tangalle",
+            location: "Sri Lanka",
+            description: " Historic port town with colonial influences and beautiful coastal scenery.."
+        }
+    ],
+    EastCoast: [
+        {
+            id:'Trincomalee',
+            img: Img13,
+            title: "Trincomalee",
+            location: "Sri Lanka",
+            description: " Historic harbor city with ancient temples and strategic naval significance.."
+        },
+        {
+            id:'Arugam',
+            img: Img14,
+            title: "Arugam Bay",
+            location: "Sri Lanka",
+            description: "Historic fishing village known for its stunning beaches and waves.."
+        },
+        {
+            id:'Pasikudah',
+            img: Img15,
+            title: "Pasikudah",
+            location: "Sri Lanka",
+            description: " Historic coastal town with pristine beaches and rich maritime heritage.."
+
+        }
+    ]
+};
+
+export default bestplaces;
