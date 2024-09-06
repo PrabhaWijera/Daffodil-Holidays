8149a111f12da043bd6ebdaa7c06da6bf7e81c69
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-09-06 21:49:48 +0530

    gallery images added

--- src/components/Gallery/galleryImages.js
+++ src/components/Gallery/galleryImages.js
@@ -1,11 +1,11 @@
-import galleryImg01 from '../../assets/images/gallery-01.jpg'
-import galleryImg02 from '../../assets/images/gallery-02.jpg'
-import galleryImg03 from '../../assets/images/gallery-03.jpg'
-import galleryImg04 from '../../assets/images/gallery-04.jpg'
-import galleryImg05 from '../../assets/images/gallery-05.jpg'
-import galleryImg06 from '../../assets/images/gallery-06.jpg'
-import galleryImg07 from '../../assets/images/gallery-07.jpg'
-import galleryImg08 from '../../assets/images/gallery-02.jpg'
+import galleryImg01 from '../../assets/places/selected images for daffodil/fishing resized.webp'
+import galleryImg02 from '../../assets/edited eliphent/resize/4.webp'
+import galleryImg03 from '../../assets/colombo/IMG_0686 copy.webp'
+import galleryImg04 from '../../assets/places/selected images for daffodil/kandyhall_resized.webp'
+import galleryImg05 from '../../assets/places/selected images for daffodil/seegiriya.webp'
+import galleryImg06 from '../../assets/places/selected images for daffodil/NewZealandFarm.webp'
+import galleryImg07 from '../../assets/edited eliphent/resize/8.webp'
+import galleryImg08 from '../../assets/colombo/IMG_0979 copy.webp'
 
 
 const galleryImages = [
